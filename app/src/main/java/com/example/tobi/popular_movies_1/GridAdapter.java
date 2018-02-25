38769156a1aee06dbc26a0e5f3cbefe33e74850d
Josh <joshua@memrise.com>
2018-02-25 18:44:57 +0000

    Create UI
    Created a basic UI template using a recycler view for the grid.

---
+++ app/src/main/java/com/example/tobi/popular_movies_1/GridAdapter.java
@@ -0,0 +1,44 @@
+package com.example.tobi.popular_movies_1;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Tobi on 25-Feb-18.
+ */
+
+public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
+    private Context context;
+
+    public GridAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.single_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
--- app/src/main/java/com/example/tobi/popular_movies_1/MainActivity.java
+++ app/src/main/java/com/example/tobi/popular_movies_1/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.tobi.popular_movies_1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private Context context;
+    private GridAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // 2 is the number of grid layout columns
+        layoutManager = new GridLayoutManager(context, 2);
+
+        // Do i need the context?
+        adapter = new GridAdapter(context);
+
+        recyclerView.setAdapter(adapter);
     }
 }
