2efaee3bd479a458407cc566e967795abfdeb69c
Joshua Onabanjo <joshua@memrise.com>
2018-03-04 15:09:47 +0000

    Retrieve the top movies via the api

--- app/src/main/java/com/example/tobi/popular_movies_1/GridAdapter.java
+++ app/src/main/java/com/example/tobi/popular_movies_1/GridAdapter.java
@@ -2,43 +2,58 @@ package com.example.tobi.popular_movies_1;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.squareup.picasso.Picasso;
+import java.util.List;
 
 /**
  * Created by Tobi on 25-Feb-18.
  */
 
 public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
-    private Context context;
+  private Context context;
+  private List<Movie> movies;
+  private String movieUrl = "https://image.tmdb.org/t/p/";
+  private String phoneSize = "w500";
 
-    public GridAdapter(Context context) {
-        this.context = context;
+  public void setMovieData(List<Movie> movies, Context context) {
+    this.movies = movies;
+    this.context = context;
+    this.notifyDataSetChanged();
+  }
+
+  @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    return new ViewHolder(
+        LayoutInflater.from(parent.getContext()).inflate(R.layout.single_item, parent, false));
+  }
+
+  @Override public void onBindViewHolder(ViewHolder holder, int position) {
+    final Movie movie = movies.get(position);
+    String path = movieUrl + phoneSize + movie.getPosterPath();
+    //https://developers.themoviedb.org/3/configuration/get-api-configuration
+    // example -> https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
+
+    // image not being passed, sucks to be me
+    Picasso.with(context).load(path).into(holder.imageView);
+    Log.d("URL", "onBindViewHolder: " + path);
+  }
+
+  @Override public int getItemCount() {
+    return movies != null ? movies.size() : 0;
+  }
+
+  public static class ViewHolder extends RecyclerView.ViewHolder {
+    @BindView(R.id.imageView) ImageView imageView;
+
+    public ViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.bind(this, itemView);
     }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.single_item, parent, false));
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView imageView;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            imageView = itemView.findViewById(R.id.imageView);
-        }
-    }
+  }
 }
--- app/src/main/java/com/example/tobi/popular_movies_1/MainActivity.java
+++ app/src/main/java/com/example/tobi/popular_movies_1/MainActivity.java
@@ -15,26 +15,30 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private Context context;
     private GridAdapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
+    //private RecyclerView.LayoutManager layoutManager;
+    private GridLayoutManager layoutManager;
     private ApiModule apiModule;
+    private MovieView movieView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+        adapter = new GridAdapter();
         context = getApplicationContext();
         recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
 
         // 2 is the number of grid layout columns
-        layoutManager = new GridLayoutManager(context, 2);
+        layoutManager = new GridLayoutManager(MainActivity.this, 2);
+        recyclerView.setLayoutManager(layoutManager);
 
         // Do i need the context?
-        adapter = new GridAdapter(context);
-
         recyclerView.setAdapter(adapter);
+        movieView = new MovieView(adapter);
 
-        init();
 
         Call <MovieResponse> call = apiModule.movieApi().getPopularMovies();
 
@@ -42,11 +46,10 @@ public class MainActivity extends AppCompatActivity {
             @Override public void onResponse(Call<MovieResponse> call,
                 Response<MovieResponse> response) {
                 MovieResponse movieResults = response.body();
-
-                for (int i = 0; i < 5; i++) {
-                    Log.d("name", movieResults.getResults().get(i).getTitle());
+                movieView.setMovieData(movieResults.getResults(), context);
+                for (int i = 0; i < movieResults.getResults().size(); i++) {
+                    Log.d("URL", "onResponse: " + movieResults.getResults().get(i).getPosterPath());
                 }
-
             }
 
             @Override public void onFailure(Call<MovieResponse> call, Throwable t) {
